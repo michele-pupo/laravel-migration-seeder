ed0924d6faa453ed2ab628be357481468c2128c5
michele-pupo <mikypupo@hotmail.it>
2024-04-10 15:25:18 +0200

    created the table structure

--- database/migrations/2024_04_10_125430_create_train_table.php
+++ database/migrations/2024_04_10_125430_create_train_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('train', function (Blueprint $table) {
             $table->id();
+            $table->string('agency', 30);
+            $table->string('departure_station', 50);
+            $table->string('arrival_station', 50);
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->smallInteger('train_code');
+            $table->tinyInteger('n_carriages');
+            $table->boolean('in_time')->default(true);
+            $table->boolean('deleted')->default(false);
             $table->timestamps();
         });
     }
