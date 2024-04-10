02084be35588ec381062e04220b3d211795115f1
michele-pupo <mikypupo@hotmail.it>
2024-04-10 18:15:22 +0200

    created function to filter trains leaving today

--- app/Http/Controllers/Train/TrainController.php
+++ app/Http/Controllers/Train/TrainController.php
@@ -3,9 +3,20 @@
 namespace App\Http\Controllers\Train;
 
 use App\Http\Controllers\Controller;
+use App\Models\Train;
 use Illuminate\Http\Request;
 
 class TrainController extends Controller
 {
-    //
+    public function index() {
+
+        // Specifica la data di interesse
+        $data = '2024-04-10';
+
+        $trainsToday = Train::whereDate('departure_time', '=', $data)->get();
+
+        dump($trainsToday);
+
+        return view('home');
+    }
 }
